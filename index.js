97d5def94d5aacf0bf8fa779786fd852af989022
Jordan Duffey <jduffey1990@gmail.com>
2023-01-18 15:44:23 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+  function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
